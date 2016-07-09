c8884f29744f99fb82ade81908ff03830ad94c85
Gregory Kulczycki <gregwk@vt.edu>
2016-07-09 01:13:56 -0400

    initial commit - the gui for the game

---
+++ adventure/GameEngine.java
@@ -0,0 +1,13 @@
+
+package adventure;
+
+/**
+ * @author Gregory K
+ */
+public interface GameEngine {
+    
+    public void initializeGame();
+    
+    public String processInput(String input);
+    
+}
