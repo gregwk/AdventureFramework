894a4e7144b6e60b19fecaf23ed8fcc6c85dc299
Kshitij Karki <Snehi@Snehis-MacBook-Air.local>
2016-07-16 23:49:07 -0400

    Initial implementation of Parser interface. Stub methods for Tokenize, VerifyWordsExistInDictionary implemented. MockDictionary instantiated in the class.

---
+++ src/adventure/ParserImpl.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adventure;
+
+import java.util.List;
+
+/**
+ *
+ * Implements the Parser interface 
+ */
+public class ParserImpl implements Parser {
+
+    Dictionary mockDictionary = new Dictionary(){
+        @Override
+        public boolean isAdjective(String word) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public boolean isNoun(String word) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public boolean isVerb(String word) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public boolean isDefined(String word) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public List<String> getActions(String verb) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public List<String> getGameObjects(List<String> objectWords) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void addGameObject(String nameId) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+            
+            };
+    
+    @Override
+    public Command parse(String userInput) {
+        
+        Command outputCommand = new Command();
+        
+        if(userInput == null || userInput.trim().isEmpty() )
+        {
+            outputCommand.errorCode = "Empty Input";
+            
+            return outputCommand;
+        }
+        
+        //1. Tokenize words
+        String[] wordTokenList = tokenizeWords(userInput);
+        
+        //2. Remove stop words
+        removeStopWords(wordTokenList);
+        
+        //3. Verify words exits in dictionary
+        verifyWordsExitsInDictionary(wordTokenList);
+        
+        return outputCommand;
+    }
+
+    /*
+    * Returns array of strings in order as entered by user
+    */
+    private String[] tokenizeWords(String userInput) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /*
+    * Removes any stop words such as articles and others ??
+    */
+    private void removeStopWords(String[] wordTokenList) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /*
+    * Verifies that all the words or group of words exits in the dictionary
+    */
+    private void verifyWordsExitsInDictionary(String[] wordTokenList) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }    
+}
