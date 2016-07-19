05cf3626f3157aafc538edac6a1c4c6f8a4347b8
VT_Yeze Li <yezeli@vt.edu>
2016-07-19 09:34:07 -0400

    Update GameWorld.java

--- src/adventure/GameWorld.java
+++ src/adventure/GameWorld.java
@@ -7,8 +7,11 @@
 package adventure;
 
 public interface GameWorld {
-	public boolean isInScope(GameObject obj);
-	public void move(GameObject obj1, GameObject obj2);
-	public void updateProperty(GameObject obj, Property prop);
-	public boolean isInInventory(GameObject obj);
+	public boolean isInScope(String objId);
+	public void move(String obj1Id, String obj2Id);
+	public void addProperty(String objId, String prop);
+	public void removeProperty(String objId, String prop);
+	public boolean containsProperty(String objId, String prop);
+	public boolean isInInventory(String objId);
 }
+
