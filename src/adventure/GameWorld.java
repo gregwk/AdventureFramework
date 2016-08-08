06df54aec3e4fe4f9b8f5b43d36392b6eadc23eb
VT_Yeze Li <yezeli@vt.edu>
2016-08-08 12:44:16 -0400

    Update GameWorld.java

--- src/adventure/GameWorld.java
+++ src/adventure/GameWorld.java
@@ -1,7 +1,5 @@
 package adventure;
 
-import java.util.List;
-
 /**
  * This interface represents all of the public operations that can be performed by the TreeGameWorld;
  * author: Yeze
@@ -77,29 +75,12 @@ public interface GameWorld {
   public void addRoom(Room room); 
   
   /**
-   * @param direction
-   */
-  public void addDirection(Direction direction);
-  
-  /**
    * Adds the property to object. 
    * 
    * @param objectId
    * @param prop
    */
-
-
-  public void removeGameObject(String objectId);
-
-/**
-   * remove relevant object   
-   * 
-   * @param obj
-   * @return 
-   */
-
-
-  public void addProperty(String objectId, GameProperty... prop);
+  public void addProperty(String objectId, String prop);
   
   /**
    * If the object contains the property.  
@@ -117,51 +98,6 @@ public interface GameWorld {
    * @param prop
    */
   public void removeProperty(String objectId, String prop);
-
-  /**
-   * Returns a List of GameObjectsIDs for all of the supplied List of objectWords
-   * 
-   * @param objectWords
-   * @return
-   */
-  public List<String> getGameObjects(List<String> objectWords);
-
-  /**
-   * Returns a string list of action IDs for the supplied verb.
-   * 
-   * @param verb
-   * @return
-   */
-  public List<String> getActions(String verb);
-
-  /**
-   * Add a GameAction which is associated with a verb
-   * 
-   * @param action
-   */
-  public void addGameAction(GameAction action);
-  
-  /**
-   * Clear the object to the state as of the constructor.
-   */
-  public void clear();
-  
-  /**
-   * Get children by object id.
-   * 
-   * @param objectId
-   * @return
-   */
-  public List<String> getChildren(String objectId);
-
-  /**
-   * Get parent by object id.
-   * 
-   * @param objectId
-   * @return
-   */
-  public String getParent(String objectId);
-  
   
 }
 
